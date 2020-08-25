7f86f7e93f367b19a8be0f166ec3f8d98299ef83
hzhan11 <xjtu_xiangxiang@hotmail.com>
2020-08-25 17:44:26 -0400

    add test script

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,10 +1,20 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,15 +27,39 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+
+    private UiDevice device;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        device.pressHome();
+        final String launcherPackage = device.getLauncherPackageName();
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), 5000);
+
+        Context context = ApplicationProvider.getApplicationContext();
+        final Intent intent = context.getPackageManager().getLaunchIntentForPackage("com.example.myapplication");
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        device.wait(Until.hasObject(By.pkg("com.example.myapplication").depth(0)), 5000);
     }
 
     @Test
     public void myTest(){
-        assertFalse("ok",false);
+        UiObject okButton = device.findObject(new UiSelector()
+                .text("Button")
+                .className("android.widget.Button"));
+        try {
+            // Simulate a user-click on the OK button, if found.
+            if (okButton.exists() && okButton.isEnabled()) {
+                okButton.click();
+            }
+        }
+        catch (UiObjectNotFoundException ex){
+            fail("test failed");
+        }
     }
 }
